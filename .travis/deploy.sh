d8d3694ab576043c537bda7aa63e63a85962893e
Loay Mohamed <loaymoh99@loaymoh99-HP-Pavilion-15-Notebook-PC>
2021-05-21 15:43:15 +0200

    Travis edited for pushing to docker hub instead of building Images everytime

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Stopping old containers.."
-sudo docker-compose stop
-echo "Removing old containers.."
-sudo docker-compose rm -f
 echo "Pulling new containers.."
 sudo docker-compose pull
+
+echo "Stopping old containers.."
+sudo docker-compose stop
+
+echo "Removing old containers.."
+sudo docker-compose rm -f
+
 echo "Reload new containers.."
 sudo docker-compose up -d
