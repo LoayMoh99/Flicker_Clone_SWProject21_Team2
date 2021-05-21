4f2309d44dabcbf5cadc9c499ba36d92e1c59799
Loay Mohamed <loaymoh99@loaymoh99-HP-Pavilion-15-Notebook-PC>
2021-05-21 14:43:44 +0200

    Travis edited for pushing to docker hub instead of building Images everytime

---
+++ .travis/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#set -e
+#git config --global push.default simple 
+#git remote add production ssh://Dev@138.91.114.14/~/BackEndScript
+#git push production master 
+docker-compose down
+docker-compose up
