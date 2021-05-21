922a8479cf80b4272c93c523aa08a77c82ce4d33
Loay Mohamed <loaymoh99@loaymoh99-HP-Pavilion-15-Notebook-PC>
2021-05-21 23:12:45 +0200

    Travis encryption added

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,12 @@
 set -e
 
 git pull origin main
+
 echo "Pulling new containers.."
 sudo docker-compose pull
 
 echo "Stopping old containers.."
-sudo docker-compose stop
-
-echo "Removing old containers.."
-sudo docker-compose rm -f
+sudo docker-compose down
 
 echo "Reload new containers.."
 sudo docker-compose up -d
