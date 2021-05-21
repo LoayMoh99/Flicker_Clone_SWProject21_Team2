51bed061d4af5f58b9439b7cb4efedaa460dbf9b
Loay Mohamed <loaymoh99@loaymoh99-HP-Pavilion-15-Notebook-PC>
2021-05-21 23:55:41 +0200

    Travis encryption added

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 #set -e
 
 pwd
-ssh -i ~/.ssh/deploy_key Dev@13.90.253.189 'cd Flicker_Clone_SWProject21_Team2/ && git clean  -d  -f . &&git reset --hard && git pull && sudo docker-compose pull && sudo docker-compose down && sudo docker-compose up -d'
-
-exit
+ssh -i ./deploy_key Dev@13.90.253.189 'cd Flicker_Clone_SWProject21_Team2/ && git clean  -d  -f . &&git reset --hard && git pull && sudo docker-compose pull && sudo docker-compose down && sudo docker-compose up -d'
