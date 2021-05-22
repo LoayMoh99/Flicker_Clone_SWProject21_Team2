a77823dc5ce998a71983dcd774075da2fa37a875
Loay Mohamed <loaymoh99@loaymoh99-HP-Pavilion-15-Notebook-PC>
2021-05-22 13:04:03 +0200

    Optimizing the efficency of Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #set -e
 
-#first clone pull the repo and pull the new images from docker hub
-#then down and up docker-compose -> reload the images (take about 15 seconds for reloading!!)
-ssh -i ./deploy_key Dev@13.90.253.189 'cd Flicker_Clone_SWProject21_Team2/ && git clean  -d  -f . &&git reset --hard && git pull && sudo docker-compose pull && sudo docker-compose down && sudo docker-compose up -d'
+#first clone pull the repo and pull the new images from docker hub ( sudo docker-compose pull &&sudo docker-compose down &&)
+#then docker-compose up -> reload the images..
+ssh -i ./deploy_key Dev@13.90.253.189 'cd Flicker_Clone_SWProject21_Team2/ && git clean  -d  -f . &&git reset --hard && git pull && sudo docker-compose up -d'
