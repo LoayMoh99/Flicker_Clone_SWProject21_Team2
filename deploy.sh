67af9f0142bb4e656810ede1e864dd8940ad485d
Loay Mohamed <loaymoh99@loaymoh99-HP-Pavilion-15-Notebook-PC>
2021-05-24 18:52:11 +0200

    SSL removed tottaly

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 
 #first clone pull the repo and pull the new images from docker hub ( sudo docker-compose pull &&sudo docker-compose down &&)
 #then docker-compose up -> reload the images..
-ssh -i ./deploy_key Dev@13.90.253.189 'cd Flicker_Clone_SWProject21_Team2/ && git clean  -d  -f . &&git reset --hard && git pull && sudo docker-compose pull && sudo docker-compose up -d'
+ssh -i ./deploy_key Dev@52.170.119.1 'cd Flicker_Clone_SWProject21_Team2/ && git clean  -d  -f . &&git reset --hard && git pull && sudo docker-compose pull && sudo docker-compose up -d'
